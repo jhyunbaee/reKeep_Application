9737bcc1293db3dd661241b203cfbae3f0b63d85
jhyunbaee <jhyunbaee@gmail.com>
2026-05-11 15:01:30 +0900

    feat: asset 페이지  구현

--- lib/asset.dart
+++ lib/asset.dart
@@ -6,7 +6,11 @@ class Asset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("자산")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("자산"),
+        backgroundColor: Colors.white,
+      ),
       body: const Center(child: Text("여기에 자산 내역을 구현하세요.")),
     );
   }
