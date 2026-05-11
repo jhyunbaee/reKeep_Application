9b73e1bf23369d0d38cdd96a8f7f964358f058f9
jhyunbaee <jhyunbaee@gmail.com>
2026-05-11 15:00:55 +0900

    feat: analysis 페이지 구현

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -6,7 +6,12 @@ class Analysis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("분석")),
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        title: const Text("분석"),
+        backgroundColor: Colors.white,
+      ),
       body: const Center(child: Text("여기에 분석 내용을 구현하세요.")),
     );
   }
